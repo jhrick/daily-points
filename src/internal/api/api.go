72f17050e8a036a6a388497adc2caceeb2127640
Jorge <hj.rickcp@gmail.com>
2024-11-09 18:02:11 -0300

    refactor: add src/ folder

---
+++ src/internal/api/api.go
@@ -0,0 +1,6 @@
+package api
+
+type AddOptions struct {
+  Title   string
+  Message string
+}
---
+++ src/internal/handler/add.go
@@ -0,0 +1,7 @@
+package handler
+
+import "github.com/jhrick/daily-points/internal/api"
+
+func Add(options api.AddOptions) error {
+  return nil
+}
